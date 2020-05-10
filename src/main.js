1edad95082d43be201b27558f46d1d7eb2e52161
Matt Hildenbrand <mhildenbrand@gmail.com>
2020-05-10 12:18:04 -0700

    adding google analytics.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from "vue";
 import VueMeta from "vue-meta";
 import App from "./App.vue";
+import VueGtag from "vue-gtag";
 
 Vue.config.productionTip = false;
 Vue.use(VueMeta);
+Vue.use(VueGtag, {
+  config: { id: "UA-8670877-1" },
+});
 
 new Vue({
   render: (h) => h(App),
